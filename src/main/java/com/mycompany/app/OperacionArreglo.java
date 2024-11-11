9c35f72349b97a8ef691afa9a06ac5290fe9d72d
curso06 <curso06@gmail.com>
2024-11-11 13:20:59 +0100

    metodo encontrar numero menor

--- src/main/java/com/mycompany/app/OperacionArreglo.java
+++ src/main/java/com/mycompany/app/OperacionArreglo.java
@@ -2,4 +2,20 @@ package com.mycompany.app;
 
 public class OperacionArreglo {
 
+    public double encontrarNumeroMenorArray( double [] arreglo){
+
+        double[] arrayInicial = arreglo;
+
+        double  menor = arrayInicial[0];
+        
+        for (int i = 1; i < arrayInicial.length; i++) {
+
+            if (arrayInicial[i] < menor){
+
+                menor = arrayInicial[i];
+            }
+
+        }
+        return menor;
+    }
 }
