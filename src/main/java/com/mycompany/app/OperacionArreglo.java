8cb1643aa843d80f94ee51ece2b906e66488517f
curso05 <notiene@notiene.es>
2024-11-11 13:22:08 +0100

    metodo encotrarMayor y test ok

--- src/main/java/com/mycompany/app/OperacionArreglo.java
+++ src/main/java/com/mycompany/app/OperacionArreglo.java
@@ -2,4 +2,14 @@ package com.mycompany.app;
 
 public class OperacionArreglo {
 
+    public double encontrarMayor(double[] arreglo) {
+
+        double numeroMayor = 0;
+        for (double d : arreglo) {
+            if (d > numeroMayor) {
+                numeroMayor = d;
+            }
+        }
+        return numeroMayor;
+    }
 }
--- src/test/java/com/mycompany/app/OperacionArregloTest.java
+++ src/test/java/com/mycompany/app/OperacionArregloTest.java
@@ -1,5 +1,18 @@
 package com.mycompany.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class OperacionArregloTest {
     
+    OperacionArreglo cut = new OperacionArreglo();
+
+    @Test
+    public void encontrarMayor () {
+        double [] ejemploArreglo = {2, 44, 54, 1, -5, 8};
+        double numeroEsperado = 54;
+        assertEquals(numeroEsperado, cut.encontrarMayor(ejemploArreglo));
+
+    }
 }
